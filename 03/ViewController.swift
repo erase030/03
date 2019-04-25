6684be02c4a019ccabacb1293fb9f17b61244ec7
dit08 <dit@imac-3.local>
2019-04-25 17:05:42 +0900

    아이폰앱개발 중간고사 문제 03번

--- 03/ViewController.swift
+++ 03/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var pv: UIPickerView!
+    
+    var color = ["흰색", "초록색", "노란색"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pv.dataSource = self
+        pv.delegate = self
+        
+        view.backgroundColor = UIColor.white
     }
-
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return color.count
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return color[row]
+    }
+    
+    
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if color[row] == "흰색" {
+            view.backgroundColor = UIColor.white
+        } else if color[row] == "초록색" {
+            view.backgroundColor = UIColor.green
+        } else {
+            view.backgroundColor = UIColor.yellow
+        }
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 100.0
+    }
 
 }
 
